8ad1f912ecdc8515418d56854cdc870efabb8667
travisgideonwong <me@travisgideonwong.com>
2024-05-24 17:33:12 +0800

    updated attractionScreen.js

--- singastays/AttractionScreen.js
+++ singastays/AttractionScreen.js
@@ -1,10 +1,154 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { globalStyles } from "./styles";
+import attractionScreen from "./assets/attractionScreen.png";
+import attractionOne from "./assets/attractionOne.jpg";
+import attractionTwo from "./assets/attractionTwo.jpg";
+import attractionThree from "./assets/attractionThree.jpg";
+import attractionFour from "./assets/attractionFour.jpg";
+import Heart from "./assets/heart.svg";
 
-export default function AttractionScreen({ navigation }) {
+export default function AttractionDetail({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>This is the Attraction Screen!</Text>
+      <ScrollView>
+        <Image source={attractionScreen} style={styles.image} />
+        <View style={styles.innerContainer}>
+          <View style={globalStyles.section}>
+            <View style={globalStyles.line}></View>
+            <Text style={globalStyles.h1}>
+              <Text>
+                Discover Comfort,{"\n"}
+                Embrace Adventure{"\n"}
+                with{" "}
+              </Text>
+              <Text style={globalStyles.span}>SingaStays</Text>
+              <Text>.</Text>
+            </Text>
+            <Text style={globalStyles.p1}>
+              Embark on a joyful stay in Singapore with SingaStays! Explore
+              tailored accommodations for a perfect blend of comfort and
+              adventure. Your unforgettable experience begins here.
+            </Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("AttractionScreen")}
+              style={globalStyles.buttonPrimaryContainer}
+              activeOpacity={1}
+            >
+              <Text style={globalStyles.buttonPrimary}>Explore Singapore</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={globalStyles.section}>
+            <Text style={[globalStyles.h2, globalStyles.textCenter]}>
+              <Text style={globalStyles.span}>Explore </Text>
+              <Text>
+                Accommodations{"\n"}
+                in Singapore{" "}
+              </Text>
+            </Text>
+            <Text style={[globalStyles.p2, globalStyles.textCenter]}>
+              Discover the perfect stay that suits your preferences. Click on the tags below to explore accommodations tailored to your needs.
+            </Text>
+          </View>
+
+          <View style={styles.stackFrame}>
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={styles.stackIndividual}
+                onPress={() => navigation.navigate("AttractionDetail")}
+                activeOpacity={1}
+              >
+                <View style={styles.stackOverlapGroup}>
+                  <Image source={attractionOne} style={styles.stackImage} />
+                  <View style={styles.heartComponent}>
+                    <Heart />
+                  </View>
+                </View>
+                <View style={styles.stackTextContent}>
+                  <Text style={styles.stackText}>Skyline Luge Sentosa</Text>
+                  <Text style={globalStyles.p2}>
+                    Discover the perfect stay that suits your preferences. Click
+                    on the tags below to explore accommodations tailored to your
+                    needs.
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.stackIndividual}
+                onPress={() => navigation.navigate("AttractionDetail")}
+                activeOpacity={1}
+              >
+                <View style={styles.stackOverlapGroup}>
+                  <Image source={attractionTwo} style={styles.stackImage} />
+                  <View style={styles.heartComponent}>
+                    <Heart />
+                  </View>
+                </View>
+                <View style={styles.stackTextContent}>
+                  <Text style={styles.stackText}>Skyline Luge Sentosa</Text>
+                  <Text style={globalStyles.p2}>
+                    Discover the perfect stay that suits your preferences. Click
+                    on the tags below to explore accommodations tailored to your
+                    needs.
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.stackIndividual}>
+                <View style={styles.stackOverlapGroup}>
+                  <Image source={attractionThree} style={styles.stackImage} />
+                  <View style={styles.heartComponent}>
+                    <Heart />
+                  </View>
+                </View>
+                <View style={styles.stackTextContent}>
+                  <Text style={styles.stackText}>Skyline Luge Sentosa</Text>
+                  <Text style={globalStyles.p2}>
+                    Discover the perfect stay that suits your preferences. Click
+                    on the tags below to explore accommodations tailored to your
+                    needs.
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.stackIndividual}>
+                <View style={styles.stackOverlapGroup}>
+                  <Image source={attractionFour} style={styles.stackImage} />
+                  <View style={styles.heartComponent}>
+                    <Heart />
+                  </View>
+                </View>
+                <View style={styles.stackTextContent}>
+                  <Text style={styles.stackText}>Skyline Luge Sentosa</Text>
+                  <Text style={globalStyles.p2}>
+                    Discover the perfect stay that suits your preferences. Click
+                    on the tags below to explore accommodations tailored to your
+                    needs.
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AttractionScreen")}
+            style={[
+              globalStyles.buttonPrimaryContainer,
+              globalStyles.textCenter,
+            ]}
+            activeOpacity={1}
+          >
+            <Text style={globalStyles.buttonPrimary}>View More</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -12,7 +156,72 @@ export default function AttractionScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#fff",
   },
+  stackFrame: {
+    marginTop: 30,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  stackIndividual: {
+    flex: 1,
+    marginHorizontal: 5,
+  },
+  stackOverlapGroup: {
+    position: "relative",
+  },
+  stackText: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 20,
+    lineHeight: 27,
+  },
+  stackImage: {
+    width: "100%",
+    height: 300,  // Fixed height for all images
+    borderRadius: 10,
+    resizeMode: 'cover',  // Ensures images cover the area uniformly
+  },
+  stackTextContent: {
+    marginTop: 20,
+  },
+  heartComponent: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 40,
+  },
+  innerContainer: {
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  rating: {
+    fontSize: 20,
+    color: "#e34b36",
+  },
+  map: {
+    width: "100%",
+    height: 322,
+    marginBottom: 20,
+  },
+  table: {
+    marginBottom: 35,
+  },
+  tableRow: {
+    borderBottomColor: "#244D3B",
+    borderBottomWidth: 1,
+    paddingTop: 14,
+    paddingBottom: 3,
+  },
+  tableCellHeader: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 16,
+  },
+  tableCell: globalStyles.p1,
 });
